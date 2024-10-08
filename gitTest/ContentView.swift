c347a03aed102a03e534d719f0a73e329fecdb3a
knockknock <yangban11111@gmail.com>
2024-10-08 14:16:50 +0900

    initial commit

---
+++ gitTest/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  gitTest
+//
+//  Created by 양승혜 on 10/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
---
+++ gitTest/gitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitTestApp.swift
+//  gitTest
+//
+//  Created by 양승혜 on 10/8/24.
+//
+
+import SwiftUI
+
+@main
+struct gitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
